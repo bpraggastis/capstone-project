97449060c68659bbc5be0b2332aedb18d24c25e2
Linnea <lkdamer@gmail.com>
2015-02-09 00:15:57 -0800

    adds delete function, and authroization_check function to comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,10 +10,25 @@ class CommentsController < ApplicationController
     end
   end
 
+  def delete
+    @comment = Comment.find(params[:id])
+    if authorization_check
+      @comment.delete
+      redirect_to User.find(session[:user_id])
+    else
+      flash[:notice] = "You are not authorized to delete that comment."
+      redirect_to root_path
+    end
+  end
+
   private
 
   def comment_params
     params.require(:comment).permit(:user_id, :content, :query_record_id)
+  end
+
+  def authorization_check
+    @comment.user_id == session[:user_id] || User.find(session[:user_id]).tier == "admin"
   end
 
 
