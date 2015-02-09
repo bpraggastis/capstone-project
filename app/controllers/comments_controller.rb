b75afc1dfcd0287b13681752cc64199bc6498f80
Linnea <lkdamer@gmail.com>
2015-02-09 00:06:08 -0800

    finishes comment create method

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,12 @@ class CommentsController < ApplicationController
 
   def create
     @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to QueryRecord.find(@comment.query_record_id)
+    elsif params[:comment][:query_record_id]
+      flash[:notice] = "Comment failed to save."
+      redirect_to QueryRecord.find(params[:comment][:query_record_id])
+    end
   end
 
   private
