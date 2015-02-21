21176fb1ea29cb033ed06bb8963891de58821adf
Linnea <lkdamer@gmail.com>
2015-02-20 16:55:20 -0800

    added method to parse date into date-picker format

--- app/models/query_record.rb
+++ app/models/query_record.rb
@@ -1,3 +1,24 @@
 class QueryRecord < ActiveRecord::Base
   belongs_to :user
+
+#  Jan+24%2C+1979
+
+  def parsed_date date
+    the_date = which_date date
+    m = the_date.strftime("%b")
+    d = the_date.strftime("%d")
+    y = the_date.strftime("%Y")
+    m + "+" + d + "%2C+" + y
+  end
+
+  private
+
+  def which_date date
+    if date == "end"
+      self.end_date
+    elsif date == "start"
+      self.start_date
+    end
+  end
+
 end
